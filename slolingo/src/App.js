eb5b8f41b171eade72bf254d5bc796505a05c2a4
Valentin Edelsbrunner <v.edelsbrunner@protonmail.com>
2025-04-13 17:16:33 +0200

    Initial Commit including gh-pages setup

---
+++ slolingo/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Hello from slolingo</h1>
+        <p>A simple React app deployed to GitHub Pages!</p>
+      </div>
+  );
+}
+
+export default App;
